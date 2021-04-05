d45779d0fb654e093e7b89f2856df4c28dc95dea
IgorMilatovic <igby.veko@gmail.com>
2021-04-06 01:17:55 +0200

    entry

--- js/opening_page_background.js
+++ js/opening_page_background.js
@@ -7,3 +7,10 @@ document.getElementById("opening-link").addEventListener("mouseover", function()
 document.getElementById("opening-link").addEventListener("mouseout", function() {
     document.getElementById("entry-cover-sepia").style.WebkitFilter = "sepia(78%) saturate(150%)";
 }, false);
+
+document.getElementById("opening-link-top").addEventListener("mouseover", function() {
+	document.getElementById("entry-cover-sepia").style.WebkitFilter = "none";
+}, false);
+document.getElementById("opening-link-top").addEventListener("mouseout", function() {
+    document.getElementById("entry-cover-sepia").style.WebkitFilter = "sepia(78%) saturate(150%)";
+}, false);
